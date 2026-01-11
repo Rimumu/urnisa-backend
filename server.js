75b11545de61eb2e54bf5934126848fa027f3715
Rimumu <48405228+Rimumu@users.noreply.github.com>
2026-01-11 20:04:06 +0700

    Update snakes in SNAKES_AND_LADDERS
    Removed some snake entries from the SNAKES_AND_LADDERS object.

--- server.js
+++ server.js
@@ -204,7 +204,7 @@ const SNAKES_AND_LADDERS = {
     // Ladders: start -> end (go UP)
     ladders: { 2: 38, 7: 14, 8: 31, 15: 26, 21: 42, 28: 84, 36: 44, 51: 67, 71: 91, 78: 98, 87: 94 },
     // Snakes: start -> end (go DOWN)
-    snakes: { 16: 6, 46: 25, 49: 11, 62: 19, 64: 60, 74: 53, 89: 68, 92: 88, 95: 75, 99: 80 }
+    snakes: { 16: 6, 46: 25, 62: 19, 64: 60, 74: 53, 92: 88, 99: 80 }
 };
 
 const roundOneDecimal = (num) => Math.round(num * 10) / 10;
