4e131053cd50bc6c5e2586fea57c616dbd1ce033
Rimumu <48405228+Rimumu@users.noreply.github.com>
2026-08-17 04:36:31 +0700

    Update URL in axios request for server health check

--- server.js
+++ server.js
@@ -5488,7 +5488,7 @@ if (MONGO_URI) {
                 console.log("🚀 Startup Deep Sync...");
                 await runSync(true);
                 setInterval(() => runSync(false), 30000);
-                setInterval(() => { axios.get('https://urnisa-dbot-m4im.onrender.com').catch(() => { }) }, 300000);
+                setInterval(() => { axios.get('https://urnisa-dbot.onrender.com').catch(() => { }) }, 300000);
             });
         })
         .catch(e => console.error("❌ DB Fail:", e));
